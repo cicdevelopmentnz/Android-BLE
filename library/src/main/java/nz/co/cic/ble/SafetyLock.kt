605b563a1ed3375c9ef75b12cbb7112e0accfc3b
Ross Leitch <Ross@metamorphicsolutions.com>
2017-03-04 09:08:36 +1300

    Added a safety

---
+++ library/src/main/java/nz/co/cic/ble/SafetyLock.kt
@@ -0,0 +1,55 @@
+package nz.co.cic.ble
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+
+/**
+ * Created by dipshit on 4/03/17.
+ */
+
+class SafetyLock(private val mActivity: Activity){
+
+    private var requstCode: Int = 1001
+    private var observerLock: ObservableEmitter<Boolean>? = null
+
+    init {
+
+    }
+
+    fun lock(): Observable<Boolean>? {
+        return Observable.create<Boolean> { subscriber ->
+            this.observerLock = subscriber
+            if(!checkPermissions()) {
+                requestPermissions()
+            }
+        }
+    }
+
+    private fun requestPermissions(){
+        ActivityCompat.requestPermissions(mActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), requstCode)
+    }
+    private fun checkPermissions(): Boolean {
+        return(ContextCompat.checkSelfPermission(mActivity.applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(mActivity.applicationContext, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED);
+    }
+
+    fun onRequestPermissionsResult(reqCode: Int, permissions: Array<String>, grantResults: Array<Int>){
+        when(reqCode){
+            requstCode -> {
+                if(grantResults.size > 0 && this.observerLock != null){
+                    this.observerLock!!.onComplete()
+                }else{
+                    this.observerLock!!.onError(Throwable("Permissions not granted"))
+                }
+                println(grantResults)
+            }
+            else -> {
+
+            }
+        }
+    }
+}
